ae4b98fd059225c737d552fe363fdb3f4423c72d
rhammer35 <rhammer35@icloud.com>
2021-04-06 19:00:05 -0400

    created UserCard class component

--- github-user-card/src/Components/UserCard.js
+++ github-user-card/src/Components/UserCard.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
 class UserCard extends React.Component {
-
+  render() {
+    return (
+      <div className='card'>
+        <img src={ this.props.user.avatar_url } alt={ this.props.user.login } />
+        <div className='card-info'>
+          <h3 className='name'>{ this.props.user.name }</h3>
+          <p className='username'>{ this.props.user.login }</p>
+          <p>Location: { this.props.user.location }</p>
+          <p>Profile: 
+            <a href={ this.props.user.html_url }>{ this.props.user.html_url }</a>
+          </p>
+          <p>Followers: { this.props.user.followers }</p>
+          <p>Following: { this.props.user.following }</p>
+          <p>Bio: { this.props.user.bio }</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default UserCard;
